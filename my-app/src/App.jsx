b3681ced8aeb8dfb41dd8ae1d4fc32154162ad43
himanshu-frontend-ind <himanshusharmavats23@gmail.com>
2026-08-02 17:17:19 +0530

    fix: use https for omdb api to fix mixed content error on vercel

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,10 +1,19 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
 // import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import MovieDetail from './pages/MovieDetail'
 
+function AppRoutes(){
+  const location = useLocation()
+  return(
+    <Routes>
+        <Route path='/' element={<Home key={location.key}/>}/>
+        <Route path='/movie/:id' element={<MovieDetail/>}/>
+      </Routes>
+  )
+}
 
 function App() {
   
@@ -13,10 +22,7 @@ function App() {
     <>
     <Router>
       <Navbar/>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/movie/:id' element={<MovieDetail/>}/>
-      </Routes>
+      <AppRoutes/>
     </Router>
     </>
   )
