01f026dc7b3cdc6c5c0761af7406e64e04b9808d
himanshu-frontend-ind <himanshusharmavats23@gmail.com>
2026-08-02 17:36:45 +0530

    fix: use https for omdb api to fix mixed content error on vercel

--- my-app/src/components/MovieCard.jsx
+++ my-app/src/components/MovieCard.jsx
@@ -2,12 +2,11 @@ import { Link } from "react-router-dom"
 
 function MovieCard({movie}) {
   return (
-    <div className="movie-card">
+    <Link to={`/movie/${movie.imdbID}`} className="movie-card">
         <img alt={movie.Title} src={movie.Poster} />
         <h3>{movie.Title}</h3>
         <p>{movie.Year}</p>
-        <Link to={`/movie/${movie.imdbID}`}>Details</Link>
-    </div>
+    </Link>
   )
 }
 
