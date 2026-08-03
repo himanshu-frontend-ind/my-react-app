167b8e4e51cdfca907d6f693339a551e22ef600a
himanshu-frontend-ind <himanshusharmavats23@gmail.com>
2026-08-03 18:59:00 +0530

    Add files via upload

---
+++ my-app/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom"
+
+function Navbar() {
+  return (
+     <nav className="navbar">
+            <Link className="logo" to="/">
+                <h2>🎬 Movie Explorer</h2>
+            </Link>
+            <Link to="/">Home</Link>
+            <Link to="/actors">Actors</Link>
+        </nav>
+  )
+}
+
+export default Navbar
