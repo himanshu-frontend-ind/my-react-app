b203640c9a2ae826a9a4a2da788cd76a1d148e65
himanshu-frontend-ind <himanshusharmavats23@gmail.com>
2026-08-04 18:11:28 +0530

    Add files via upload

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,24 +1,23 @@
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
-// import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import MovieDetail from './pages/MovieDetail'
 import Actors from './components/Actors'
+import ActorMovies from './components/ActorMovies'
 
 function AppRoutes(){
   return(
     <Routes>
         <Route path='/' element={<Home />}/>
         <Route path="/actors" element={<Actors />} />
+        <Route path="/actor/:name" element={<ActorMovies />} />
         <Route path='/movie/:id' element={<MovieDetail/>}/>
       </Routes>
   )
 }
 
 function App() {
-  
-
   return (
     <>
     <Router>
